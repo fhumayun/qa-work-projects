c8194c55d0549721fa015eda36d472fde81d0d33
John Shanahan <jshanahan@eagleeyeintelligence.com>
2016-11-17 16:03:26 -0500

    Fixed git diff command which would break jenkins due to it not letting go of the terminal. Fixed by piping output to tee.

--- deployment_fabric_script/fabfile.py
+++ deployment_fabric_script/fabfile.py
@@ -17,24 +17,24 @@ def uname():
 
 def make_deploy_srm():
     # Deploy straxRM
-    run('cd /usr/local/bin/OpsDeck/straxRM && git diff && make deploy')
+    run('cd /usr/local/bin/OpsDeck/straxRM && git diff | tee && make deploy')
 
 def make_deploy_sac():
     # Deploy SAC
-    run('cd /usr/local/bin/OpsDeck/eesac && git diff && make deploy')
+    run('cd /usr/local/bin/OpsDeck/eesac && git diff | tee && make deploy')
 
 def make_deploy_api():
     # Deploy apiserver
-    run('cd /usr/local/bin/OpsDeck/apiserver && git diff && make deploy')
+    run('cd /usr/local/bin/OpsDeck/apiserver && git diff | tee && make deploy')
 
 def make_deploy_id():
     # Deploy idserver
-    run('cd /usr/local/bin/OpsDeck/straxID && git diff && make deploy')
+    run('cd /usr/local/bin/OpsDeck/straxID && git diff |tee && make deploy')
 
 def make_deploy_media():
     # Deploy mediaserver
-    run('cd /usr/local/bin/OpsDeck/media/strax_media && git diff && make deploy')
+    run('cd /usr/local/bin/OpsDeck/media/strax_media && git diff | tee && make deploy')
 
 def make_deploy_playback():
     # Deploy playback
-    run('cd /usr/local/bin/OpsDeck/playback && git diff && make deploy')
+    run('cd /usr/local/bin/OpsDeck/playback && git diff | tee && make deploy')
