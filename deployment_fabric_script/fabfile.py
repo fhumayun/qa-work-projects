c235a68275505dbb756f66a65e8343ac45c2fe00
John Shanahan <jshanahan@eagleeyeintelligence.com>
2016-11-17 13:39:36 -0500

    Added fabric file to do deployments on all environments

---
+++ deployment_fabric_script/fabfile.py
@@ -0,0 +1,40 @@
+# Continuous Delivery Fabric File
+# John Shanahan
+
+import imp
+try:
+    imp.find_module('fabric')
+    from fabric.api import run
+except ImportError:
+    print('\033[91m')
+    print('# Fabric is not installed. Please `pip install fabric`')
+    print('# Exiting ...')
+    print('\033[0m')
+    exit()
+
+def uname():
+    run('uname -a')
+
+def make_deploy_srm():
+    # Deploy straxRM
+    run('cd /usr/local/bin/OpsDeck/straxRM && git diff && make deploy')
+
+def make_deploy_sac():
+    # Deploy SAC
+    run('cd /usr/local/bin/OpsDeck/eesac && git diff && make deploy')
+
+def make_deploy_api():
+    # Deploy apiserver
+    run('cd /usr/local/bin/OpsDeck/apiserver && git diff && make deploy')
+
+def make_deploy_id():
+    # Deploy idserver
+    run('cd /usr/local/bin/OpsDeck/straxID && git diff && make deploy')
+
+def make_deploy_media():
+    # Deploy mediaserver
+    run('cd /usr/local/bin/OpsDeck/media/strax_media && git diff && make deploy')
+
+def make_deploy_playback():
+    # Deploy playback
+    run('cd /usr/local/bin/OpsDeck/playback && git diff && make deploy')
