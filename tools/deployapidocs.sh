179f5d97e050533c5598b0482e6ea676697245df
John Shanahan <jshanahan@eagleeyeintelligence.com>
2017-02-01 10:33:11 -0500

    Fixed deployapidocs.sh to check for all required dependancies before running

--- tools/deployapidocs.sh
+++ tools/deployapidocs.sh
@@ -4,21 +4,34 @@ echo '================================================'
 echo "$0"
 echo '================================================'
 
-echo "Creating apidocs..."
-
 if [[ -n /tmp/api ]]; then
     rm -rf /tmp/api
     echo "Removing stale api files..."
 fi
 
+echo "Grabbing latest code..."
 git clone https://github.com/groupcaretech/sprouttrax /tmp/api
 cd /tmp/api
 
+echo "Checking node installed..."
+if [[ $(which node | wc -l | xargs) < 1 ]]; then
+    echo "Please install 'node' and try again..."
+    exit 1
+fi
+
+echo "Checking npm installed..."
+if [[ $(which npm | wc -l | xargs) < 1 ]]; then
+    echo "Please install 'npm' and try again..."
+    exit 1
+fi
+
+echo "Checking apidoc installed..."
 if [[ $(npm list -g 2>/dev/null | grep apidoc | wc -l | xargs) < 1 ]]; then
     echo "Please install apidoc (npm install -g apidoc)..."
     exit 1
 fi
 
+echo "Creating apidocs..."
 apidoc -i app/controllers/ -o apidoc/
 
 if [[ $(docker ps -a | grep apidocs | wc -l | xargs) > 0 ]]; then
@@ -26,6 +39,7 @@ if [[ $(docker ps -a | grep apidocs | wc -l | xargs) > 0 ]]; then
     echo "Removing stale apidocs container..."
 fi
 
+echo "Starting docker container..."
 docker run --name apidocs -v /tmp/api/apidoc:/usr/share/nginx/html:ro -p 3270:80 -P -d nginx
 
 echo "Done..."
