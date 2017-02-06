0dd5fb7851ecffaeff0df831a273546966f18a43
John Shanahan <jshanahan@eagleeyeintelligence.com>
2017-02-06 15:55:59 -0500

    Created install script for copying our tools into usr local bin

---
+++ tools/install_tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo '================================================'
+echo "$0"
+echo '================================================'
+
+echo 'Copying buildnum.sh...'
+cp buildnum.sh /usr/local/bin
+chmod +x /usr/local/bin/buildnum.sh
+
+echo 'Done...'
