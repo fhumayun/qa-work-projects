4dba82bc41ad1b81b8b83611dbfacdebd601ed04
John Shanahan <jshanahan@eagleeyeintelligence.com>
2017-02-06 13:21:49 -0500

    Added dependancy check in entrypoint.sh for cukes

--- Kaizen/srm/entrypoint.sh
+++ Kaizen/srm/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 echo '[INFO] entrypoint.sh started...'
 
+# Make sure req's are installed
+echo "Checking node installed..."
+if [[ $(which node | wc -l | xargs) < 1 ]]; then
+    echo "Please install 'node' and try again..."
+    exit 1
+fi
+
+# Deps
+npm i
+
+# Cuke
+npm i -g cucumberjs
+
 # Go to tests dir and execute the runner.
 cd tests
 
