9db76656b4571e9f496fbe528e8a8796c83bf36b
John Shanahan <jshanahan@eagleeyeintelligence.com>
2017-03-24 11:35:47 -0400

    Modified buildnum and removed old chef scripts

--- tools/shell-scripts/buildnum.sh
+++ tools/shell-scripts/buildnum.sh
@@ -1,6 +1,113 @@
 #!/bin/bash
 
-#Uncomment set -x command to display verbose debugging
+# --- Lib
+
+# Version Override feature
+override(){
+    rule "${bold}${red}=${reset}"
+    filePattern="[0-9].[0-9]"
+    tmpDir="tmp"
+    overrideFile="/$tmpDir/$filePattern"
+    if [ ! -f "${overrideFile}" ]; then
+        echo "${bold}Using Default Branch version: ${bold}${green}$GitNewRelease ${reset}"
+    else
+        newBranchVersion=$(echo /tmp/[0-9].[0-9] | cut -d'/' -f3)
+        echo "${bold}${yellow}Branch Version Override Detected:${reset} ${bold}${green}$newBranchVersion${reset}"
+        export GitNewRelease=${newBranchVersion}
+    fi
+    rule "${bold}${red}=${reset}"
+}
+
+
+# Print a horizontal rule
+rule() {
+    printf -v _hr "%*s" $(tput cols) && echo ${_hr// /${1--}}
+}
+
+
+# Print horizontal ruler with message
+rulem ()  {
+    # Fill line with ruler character ($2, default "-"), reset cursor, move 2 cols right, print message
+    printf -v _hr "%*s" $(tput cols) && echo -en ${_hr// /${2--}} && echo -e "\r\033[2C$1"
+}
+
+
+# Buildnum help message
+buildnum_help() {
+    rule "${bold}${red}=${reset}"
+    # Print usage
+    echo "Usage:  ./buildnum.sh <workdir> <jira>"
+    echo ""
+    echo "Strax version number generator"
+    echo ""
+    echo "Options:"
+    echo "  help - Print out help message"
+    echo "  <dir> - Specify directory other than self (default 'pwd')"
+    echo "  <jira> - Specify whether to send new version number to jira or not (default yes)"
+    echo "${bold}${yellow}usage: First argument (required) $(basename $0) path of Strax Repo${reset}"
+    echo "${bold}${green}Second argument to send version number to Jira${reset}"
+    echo "${bold}${blue}example: $(basename $0) . 1${reset}"
+    rule "${bold}${red}=${reset}"
+}
+
+
+# escape slashes
+function escape_slashes {
+    sed 's/\//\\\//g'
+}
+
+
+# change line
+function change_line {
+    if [ "$(uname)" == "Darwin" ]; then
+        # Do something under Mac OS X platform
+        local OLD_LINE_PATTERN=${SearchTerm}
+        local NEW_LINE=${FriendlyVer}
+        local FILE=${FileToSearch}
+        local NEW=$(echo "${NEW_LINE}" | escape_slashes)
+        sed -i .bak '/'"${OLD_LINE_PATTERN}"'/s/.*/'"${NEW}"'/' "${FILE}"
+        mv "${FILE}.bak" /tmp/
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        # Do something under GNU/Linux platform
+        sed -i "/VERSION/cENV VERSION ${Prefix}.${GitNewRelease}.${GitHead}.${GitNameFixed} (${BuildDate})" ${FileToSearch}
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+        # Do something under Windows NT platform
+        echo "${bold}${red}FAIL!!${reset} $(basename) is not ready for Windows"
+    fi
+}
+
+
+# Fix name
+FixName(){
+    export GitRepoName=$(basename $(git remote show -n origin | grep Fetch | cut -d: -f2- | tr '[:lower:]' '[:upper:]' ))
+    if [ "$GitRepoName" == "STRAXID" ]; then
+        export GitRepoName="ID"
+    fi
+    if [ "$GitRepoName" == "STRAXMEDIA" ]; then
+        export GitRepoName="MEDIA"
+    fi
+    if [ "$GitRepoName" == "SACPLAYBACK" ]; then
+        export GitRepoName="PB"
+    fi
+    if [ "$GitRepoName" == "EAGLEEYESAC" ]; then
+        export GitRepoName="SAC"
+    fi
+    if [ "$GitRepoName" == "STRAXRM" ]; then
+        export GitRepoName="SRM"
+    fi
+    if [ "$GitRepoName" == "SPROUTTRAX" ]; then
+        export GitRepoName="API"
+    fi
+    if [ "$GitRepoName" == "STRAX-APP" ]; then
+        # Todo Fix this?
+        export GitRepoName="STRAX-APP"
+    fi
+    echo ${GitRepoName}
+}
+
+
+# --- Setup
+
 #set -x
 
 # Terminal colors
@@ -12,108 +119,31 @@ reset=$(tput sgr0)
 dim=$(tput dim)
 bold=$(tput bold)
 
-#Capture directory passed as argument.
-workdir=$(dirname "$1")
-jiraflag=$2
+# Grab command line args
 
-# Version Override feature
-override(){
-	rule ${bold}${red}=${reset}
-	filePattern="[0-9].[0-9]"
-	tmpDir="tmp"
-	overrideFile="/$tmpDir/$filePattern"
-	if [ ! -f ${overrideFile} ]
-	then
-		echo "${bold}${white}Using Default Branch version: ${bold}${green}$GitNewRelease ${reset}"
-	else
-		newBranchVersion=$(echo /tmp/[0-9].[0-9] | cut -d'/' -f3)
-		echo "${bold}${yellow}Branch Version Override Detected:${reset} ${bold}${green}$newBranchVersion${reset}"
-		# Extract version digits
-		# export Major=$(echo ${overrideFile}| cut -d'.' -f1)
-		# export Minor=$(echo ${overrideFile}| cut -d'.' -f1)
-		export GitNewRelease=$newBranchVersion
-	fi
-	rule ${bold}${red}=${reset}
-}
-
-# Print a horizontal rule
-rule() {
-	printf -v _hr "%*s" $(tput cols) && echo ${_hr// /${1--}}
-}
-
-# Print horizontal ruler with message
-rulem ()  {
-	# Fill line with ruler character ($2, default "-"), reset cursor, move 2 cols right, print message
-	printf -v _hr "%*s" $(tput cols) && echo -en ${_hr// /${2--}} && echo -e "\r\033[2C$1"
-}
-
-
-
-# Making a Help feature
-
-help() {
-	rule ${bold}${red}=${reset}
-	echo "${bold}${yellow}usage: First argument (required) $(basename $0) path of Strax Repo${reset}"
-	echo "${bold}${green}Second argument to send version number to Jira${reset}"
-	echo "${bold}${blue}example: $(basename $0) . 1${reset}"
-    rule ${bold}${red}=${reset}
-}
-
-function escape_slashes {
-    sed 's/\//\\\//g' 
-}
-
-function change_line {
-	if [ "$(uname)" == "Darwin" ]; then
-    	# Do something under Mac OS X platform
-		local OLD_LINE_PATTERN=${SearchTerm}
-		local NEW_LINE=${FriendlyVer}
-		local FILE=${FileToSearch}
-		local NEW=$(echo "${NEW_LINE}" | escape_slashes)
-		sed -i .bak '/'"${OLD_LINE_PATTERN}"'/s/.*/'"${NEW}"'/' "${FILE}"
-		mv "${FILE}.bak" /tmp/
-	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-		# Do something under GNU/Linux platform
-		sed -i "/VERSION/cENV VERSION ${Prefix}.${GitNewRelease}.${GitHead}.${GitNameFixed} (${BuildDate})" $FileToSearch
-	elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-		# Do something under Windows NT platform
-		echo "${bold}${red}FAIL!!${reset} $(basename) is not ready for Windows"
-	fi
-}
-
-if [ $# -lt 1 ]; then
-	help
-	exit 1
+# If help
+if [ "$1" == "help" ]; then
+    buildnum_help
+    exit 0
 fi
 
-# Common Variables
+# Capture work directory or use current dir if not specified
+if [[ -z "$1" ]]; then
+    workdir=$(pwd)
+else
+    workdir=$(dirname "$1")
+fi
+
+# Capture the "send version to jira" flag or default to "yes"
+if [[ -z "$2" ]]; then
+    jiraflag=1
+else
+    jiraflag=$2
+fi
+
+# Version generation vars
 export Prefix="STX"
-
-# Get git commit count
 export GitHead=$(git rev-list master HEAD --count)
-
-FixName(){
-	export GitRepoName=$(basename $(git remote show -n origin | grep Fetch | cut -d: -f2- | tr '[:lower:]' '[:upper:]' ))
-	if [ "$GitRepoName" == "STRAXID" ]; then 
-		export GitRepoName="ID"
-    fi
-	if [ "$GitRepoName" == "STRAXMEDIA" ]; then 
-		export GitRepoName="MEDIA"
-    fi
-	if [ "$GitRepoName" == "SACPLAYBACK" ]; then 
-		export GitRepoName="PB"
-    fi
-	if [ "$GitRepoName" == "EAGLEEYESAC" ]; then 
-		export GitRepoName="SAC"
-	fi
-	if [ "$GitRepoName" == "STRAXRM" ]; then 
-		export GitRepoName="SRM"
-	fi
-	if [ "$GitRepoName" == "SPROUTTRAX" ]; then 
-		export GitRepoName="API"
-	fi
-	echo $GitRepoName
-}
 export GitNewRelease=$(basename $(git branch -a | grep "remotes" | grep "v[0-9].*" | awk -F "/" '{ print $3 }' | cut -dv -f2- | tr -d '\r' | tail -1))
 override
 export BuildDateTime=$(date +"%Y%m%d.%H%M")
@@ -123,16 +153,22 @@ export SearchTerm="VERSION"
 export GitNameFixed=$(FixName)
 export FriendlyVer="ENV VERSION ${Prefix}.${GitNewRelease}.${GitHead}.${GitNameFixed} (${BuildDate})"
 
-# Inert Variables
-export Major=0
-export Minor=$PT_IT
 
-# Inert feature: Get current sprint iteration from pivotal tracker
-export TOKEN='996c058ea4864d8ba211a5f445285230'
-export PROJECT_ID=1202356
-PT_IT=$(curl -sS -X GET -H "X-TrackerToken: $TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$PROJECT_ID/iterations?limit=1&scope=current" | grep '"number"' | awk -F":" {'print $2'} | cut -d, -f1-1 | sed -e 's/^[[:space:]]*//')
+# --- Main
 
-echo "Current Sprint Number:${blue} $PT_IT ${reset}"
+# Check Dockerfile exists
+if [ ! -f $(pwd)/${FileToSearch} ]; then
+    echo "${bold}${red}FAIL!!${reset} $FileToSearch not found! Unable to Patch Build Number!"
+    exit 1
+fi
+
+# Check "VERSION" string is in Dockerfile
+if ! grep -q ${SearchTerm} ${FileToSearch}; then
+    echo "${bold}${red}FAIL!!${reset} $SearchTerm not found in $FileToSearch"
+    exit 1
+fi
+
+# Print debug info
 echo "Git Head Counter: ${blue} $GitHead ${reset}"
 echo "Git Repo Name: ${blue} $GitNameFixed ${reset}"
 echo "Git Release Branch: ${blue} $GitNewRelease ${reset}"
@@ -145,56 +181,42 @@ echo "$Prefix.$GitNewRelease.$GitHead.$GitNameFixed" >build.prop
 rule ${bold}${green}=${reset}
 echo "${dim}Loading Build Number:${reset}${bold}${yellow} $Prefix.$GitNewRelease.$GitHead.$GitNameFixed ${reset}${dim} in $FileToSearch ${reset}"
 rule ${bold}${green}=${reset}
-if [ -f $workdir/$FileToSearch ]; then
-	#echo -e "\e[1m\e[92m\e[5mPASS!!\e[0m Dockerfile found! Patching Build Number..."
-	echo "${bold}${green}PASS!!${reset} $FileToSearch found"
-	if grep -q ${SearchTerm} ${FileToSearch}; then
-		export DockerfileOldVersion=$( cat "$FileToSearch" | grep "$SearchTerm" )
-   		echo "${bold}${green}PASS!!${reset} Version Info found in $FileToSearch"
-	    echo "Before: ${bold}${yellow} ${DockerfileOldVersion} ${reset}"
-		change_line
-		export DockerfileNewVersion=$( cat "$FileToSearch" | grep "$SearchTerm" )
-		echo "Now: ${bold}${green} ${DockerfileNewVersion} ${reset}"
-	else
-   		echo "${bold}${red}FAIL!!${reset} $SearchTerm not found in $FileToSearch"
-		exit
-	fi
-	
-	if [[ -z ${jiraflag} && ${jiraflag} == "" ]]; then
-		echo "${bold}${red}** Not Sending Build Number to Jira **${reset}"
-	else
-		if [[ ${jiraflag} -eq 1 ]]; then
-			rule ${bold}${red}=${reset}
-			echo "${bold}${blue} Sending Build Number:${reset}${bold}${yellow} $Prefix.$GitNewRelease.$GitHead.$GitNameFixed ($BuildDate) ${reset} to JIRA"
-			rule ${bold}${red}=${reset}
 
-			# Test variable scope
-			#echo "Prefix Keyword: ${red} $Prefix ${reset}"
-			#echo "Major: ${red} $Release ${reset}"
-			#echo "Release: ${red} $Release ${reset}"
-			#echo "GitName: ${red} $GitRepo ${reset}"
-			#echo "Date/Time: ${red} $DateTime ${reset}"
+# Patch the VERSION variable in Dockerfile
+export DockerfileOldVersion=$( cat "$FileToSearch" | grep "$SearchTerm" )
+echo "${bold}${green}PASS!!${reset} Version Info found in $FileToSearch"
+echo "Before: ${bold}${yellow} ${DockerfileOldVersion} ${reset}"
+change_line
+export DockerfileNewVersion=$( cat "$FileToSearch" | grep "$SearchTerm" )
+echo "Now: ${bold}${green} ${DockerfileNewVersion} ${reset}"
 
-			# Get develop branch sha1 to fork new branch from
-			VersionString="$Prefix.$GitNewRelease.$GitHead.$GitNameFixed ($BuildDate)"
-			echo "VERSIONSTRING=${bold}${red} $Prefix.$GitNewRelease.$GitHead.$GitNameFixed ($BuildDate) ${reset}"
 
-			# Call Jira to create new Verion.
-			CurlURL="https://eagleeyeintel.atlassian.net/rest/api/2/version"
-			CurlType="Content-Type: application/json"
-			CurlAuth="Authorization: Basic Zmh1bWF5dW5AZ3JvdXBjYXJldGVjaC5jb206Y2FyZXRlYW0="
-			CurlCache="Cache-Control: no-cache"
-			CurlDATA='{
-                        	"project": "'${Prefix}'",
-                        	"name" : "'${VersionString}'",
-                        	"description": "",
-                        	"userReleaseDate": "",
-                        	"userStartDate": ""
-                    	}'
-			CurlResponse=$(curl -s -g -X POST -H "$CurlType" -H "$CurlAuth" -H "$CurlCache" -d "$CurlDATA" "$CurlURL")
-		fi
-	fi
+# Do/dont sent new version to Jira releases page
+# Todo Should the "-z jiraflag && ..." be a "&&" or "||" ?
+if [[ -z ${jiraflag} && ${jiraflag} == "" ]]; then
+    echo "${bold}${red}** Not Sending Build Number to Jira **${reset}"
 else
-	#echo -e "\e[31m\e[1m\e[5mFAIL!!\e[0m $FileToSearch not found! Unable to Patch Build Number!"
-	echo "${bold}${red}FAIL!!${reset} $FileToSearch not found! Unable to Patch Build Number!"
+    if [[ ${jiraflag} -eq 1 ]]; then
+        rule ${bold}${red}=${reset}
+        echo "${bold}${blue} Sending Build Number:${reset}${bold}${yellow} $Prefix.$GitNewRelease.$GitHead.$GitNameFixed ($BuildDate) ${reset} to JIRA"
+        rule ${bold}${red}=${reset}
+
+        # Get develop branch sha1 to fork new branch from
+        VersionString="$Prefix.$GitNewRelease.$GitHead.$GitNameFixed ($BuildDate)"
+        echo "VERSIONSTRING=${bold}${red} $Prefix.$GitNewRelease.$GitHead.$GitNameFixed ($BuildDate) ${reset}"
+
+        # Call Jira to create new Verion.
+        CurlURL="https://eagleeyeintel.atlassian.net/rest/api/2/version"
+        CurlType="Content-Type: application/json"
+        CurlAuth="Authorization: Basic Zmh1bWF5dW5AZ3JvdXBjYXJldGVjaC5jb206Y2FyZXRlYW0="
+        CurlCache="Cache-Control: no-cache"
+        CurlDATA='{
+                        "project": "'${Prefix}'",
+                        "name" : "'${VersionString}'",
+                        "description": "",
+                        "userReleaseDate": "",
+                        "userStartDate": ""
+                    }'
+        CurlResponse=$(curl -s -g -X POST -H "$CurlType" -H "$CurlAuth" -H "$CurlCache" -d "$CurlDATA" "$CurlURL")
+    fi
 fi
