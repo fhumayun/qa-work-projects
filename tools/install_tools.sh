e1491a422be7c5ca96ea040e7190e922f50f4129
QA <qa@groupcaretech.com>
2017-05-15 21:25:03 +0000

    Changed up order of modifications

--- tools/install_tools.sh
+++ tools/install_tools.sh
@@ -3,8 +3,22 @@
 echo "[START]"
 echo ''
 
+# Install buildnum.sh
+echo '[0] [INSTALL] buildnum.sh...'
+cp shell-scripts/buildnum.sh /usr/local/bin
+chmod +x /usr/local/bin/buildnum.sh
+if [[ $? -eq 0 ]]; then echo -e "[0] ✅"; else echo -e "[0] ❌"; fi
+echo ''
+
+# Install dropbox_uploader.sh
+echo '[1] [INSTALL] dropbox_uploader.sh...'
+curl -s -S "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o /usr/local/bin/dropbox_uploader.sh
+chmod +x /usr/local/bin/dropbox_uploader.sh
+if [[ $? -eq 0 ]]; then echo -e "[1] ✅"; else echo -e "[1] ❌"; fi
+echo ''
+
 # Install oh-my-zsh 
-echo '[0] [INSTALL] zsh...'
+echo '[3] [INSTALL] zsh...'
 sudo apt-get install -y zsh toilet cowsay
 sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 sudo sed -i "s@/bin/bash@/usr/bin/zsh@" /etc/passwd
@@ -12,28 +26,14 @@ if [[ $? -eq 0 ]]; then echo -e "[1] ✅"; else echo -e "[1] ❌"; fi
 echo ''
 
 # Install dotfiles
-echo '[1] [INSTALL] zsh resource files...'
+echo '[4] [INSTALL] zsh resource files...'
 cp .zshrc ~/
 cp .zprofile ~/
 if [[ $? -eq 0 ]]; then echo -e "[0] ✅"; else echo -e "[0] ❌"; fi
 echo ''
 
-# Install buildnum.sh
-echo '[2] [INSTALL] buildnum.sh...'
-cp shell-scripts/buildnum.sh /usr/local/bin
-chmod +x /usr/local/bin/buildnum.sh
-if [[ $? -eq 0 ]]; then echo -e "[0] ✅"; else echo -e "[0] ❌"; fi
-echo ''
-
-# Install dropbox_uploader.sh
-echo '[3] [INSTALL] dropbox_uploader.sh...'
-curl -s -S "https://raw.githubusercontent.com/andreafabrizi/Dropbox-Uploader/master/dropbox_uploader.sh" -o /usr/local/bin/dropbox_uploader.sh
-chmod +x /usr/local/bin/dropbox_uploader.sh
-if [[ $? -eq 0 ]]; then echo -e "[1] ✅"; else echo -e "[1] ❌"; fi
-echo ''
-
 # add timestamps to history
-echo '[4] [WARNING] *!* This will NOT be automatically run *!*'
+echo '[2] [WARNING] *!* This will NOT be automatically run *!*'
 echo "     \`---- Please manually run \`sudo $(pwd)/setup/historytimestamp.sh\`"
 echo ''
 
