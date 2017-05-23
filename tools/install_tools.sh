d67dc0c4818e52125d2583ceed291bcef213f38d
gct-faisal <fhumayun@groupcaretech.com>
2017-05-23 16:46:21 -0400

    improving buildnum deployment logic

--- tools/install_tools.sh
+++ tools/install_tools.sh
@@ -5,10 +5,37 @@ echo ''
 
 # Install buildnum.sh
 echo '[0] [INSTALL] buildnum.sh...'
-sudo ln -s shell-scripts/buildnum.sh /usr/local/bin/
-sudo chmod +x /usr/local/bin/buildnum.sh
-if [[ $? -eq 0 ]]; then echo -e "[0] ✅"; else echo -e "[0] ❌"; fi
-echo ''
+# Remove previous file copies and ensure symlink is establish instead
+shelldir="shell-scripts"
+ulbDir="/usr/local/bin"
+ulbDirFile="buildnum.sh"
+ulbDirPath="${ulbDir}/${ulbDirFile}"
+if [ -L ${ulbDirPath} ] ; then
+   if [ -e ${ulbDirPath} ] ; then
+      echo "✅ Good link"
+   else
+      echo " ❌ Broken link"
+      sudo ln -s ${shelldir}/${ulbDirFile} ${ulbDir}
+      sudo chmod +x ${ulbDirPath}
+        if [[ $? -eq 0 ]]; then echo -e "[0] ✅"; else echo -e "[0] ❌"; fi
+        echo ''
+   fi
+elif [ -e ${ulbDirPath} ] ; then
+   echo " ❌ Not a link"
+   echo "Replacing with symlink"
+   sudo rm -f ${ulbDirPath}
+   sudo ln -s ${shelldir}/${ulbDirFile} ${ulbDir}
+   sudo chmod +x ${ulbDirPath}
+     if [[ $? -eq 0 ]]; then echo -e "[0] ✅"; else echo -e "[0] ❌"; fi
+     echo ''
+else
+   echo " ❌ Missing"
+   echo "Adding buildnum symlink"
+   sudo ln -s ${shelldir}/${ulbDirFile} ${ulbDir}
+   sudo chmod +x ${ulbDirPath}
+     if [[ $? -eq 0 ]]; then echo -e "[0] ✅"; else echo -e "[0] ❌"; fi
+     echo ''
+fi
 
 # Install dropbox_uploader.sh
 echo '[1] [INSTALL] dropbox_uploader.sh...'
