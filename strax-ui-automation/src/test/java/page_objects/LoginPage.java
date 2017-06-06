411709b23ed5c1c9fb2c304d586546e2e4dd208d
yghumade <yogiraj.ghumade@gmail.com>
2017-06-06 11:34:57 +0530

    fix for variable key issue

--- strax-ui-automation/src/test/java/page_objects/LoginPage.java
+++ strax-ui-automation/src/test/java/page_objects/LoginPage.java
@@ -1,6 +1,7 @@
 package page_objects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
