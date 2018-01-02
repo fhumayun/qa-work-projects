b2b12c7c93b61aac20b3c5bf4db7ce9ffd175b63
yghumade <yogiraj.ghumade@gmail.com>
2018-01-02 15:36:21 +0530

    fixing the error related to wrapperUtility

--- strax-ui-automation/src/test/java/page_objects/CommonClass.java
+++ strax-ui-automation/src/test/java/page_objects/CommonClass.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utils.PropertiesFileReader;
-import utils.WrapperUtility;
 
 public class CommonClass{
 	
