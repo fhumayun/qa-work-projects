dee16d3d89ba47eca400122b03b87822188610b9
Tim McClure <tmcclure@groupcaretech.com>
2019-06-14 10:49:25 -0400

    added display vehicle hits

---
+++ vehicle_analytics/displayHits.js
@@ -0,0 +1,42 @@
+// import MQTT.js
+const mqtt = require('mqtt');
+// <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
+// const mqtt = require('mqtt')
+
+// WebSocket connect url
+const WebSocket_URL = 'wss://sandbox.strax.co/mqtt';
+
+// TCP/TLS connect url
+const TCP_URL = 'mqtt://localhost:1883'
+const TCP_TLS_URL = 'mqtts://localhost:8883'
+
+
+const options = {
+  connectTimeout: 4000,
+
+  // Authentication
+  clientId: 'strax-ier-service-display',
+  username: 'admin',
+  password: 'public',
+  keepalive: 60,
+  clean: true,
+};
+
+const client = mqtt.connect(WebSocket_URL, options);
+
+// after connect
+client.on('connect', () => {
+  console.log('Connected to', WebSocket_URL);
+
+  client.subscribe('0/PBSO/ier-service/trigger/capture/#', (err) => {
+    console.log(err || 'Subscribe Success')
+  });
+});
+
+// handle message event
+client.on('message', (topic, message) => {
+  message = JSON.parse(message.toString('utf-8'));
+  for (let i=0; i<message.length; i++) {
+    console.log(`${message[i].triggerType} ${JSON.stringify(message[i].keywords)}  ${new Date(message[i].time*1000)}`);
+  }
+});
