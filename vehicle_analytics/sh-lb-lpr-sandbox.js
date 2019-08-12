bcdc3adfa64e40c45f6c3cc5d3c21ba34e652a03
lborn <lborn@eagleeyeintelligence.com>
2019-08-12 12:54:46 -0400

    send hits for staging and sandbox

---
+++ vehicle_analytics/sh-lb-lpr-sandbox.js
@@ -0,0 +1,67 @@
+// import MQTT.js
+const mqtt = require('mqtt');
+// <script src="https://unpkg.com/mqtt/dist/mqtt.min.js"></script>
+// const mqtt = require('mqtt')
+
+// WebSocket connect url
+//Change for env switching
+/*
+ENV 
+Staging             'wss://staging.strax.co/mqtt'
+CD                  'wss://cd.strax.co/mqtt'
+Sandbox             'wss://sandbox.strax.co/mqtt'
+*/
+const WebSocket_URL = 'wss://sandbox.strax.co/mqtt';
+// const WebSocket_URL = "ws://localhost:8083/mqtt"; //local
+
+// TCP/TLS connect url
+const TCP_URL = 'mqtt://localhost:1883'
+const TCP_TLS_URL = 'mqtts://localhost:8883'
+
+const CAPTURE_TOPIC = '0/PBSO/ier-service/trigger/capture'
+const ALERT_TOPIC = '0/PBSO/ier-service/trigger/alert/#'//hits
+
+const options = {
+  connectTimeout: 4000,
+
+  // Authentication
+  clientId: 'strax-ier-service-lbs111',
+  username: 'admin',
+  password: 'public',
+  keepalive: 60,
+  clean: true,
+};
+
+const client = mqtt.connect(WebSocket_URL, options);
+
+//Copy from NoSQLBooster for MongoDB free edition. This message does not appear if you are using a registered version.
+
+//Faisals car
+let hit = [ { lpScore: 0.6981093287467957,
+    time: 1561648358,
+    keys: [ 'ierhit/5ce5685332da7f002bc14a64/1561648357544.jpeg' ],
+    x: 1029,
+    keywords: [{"lpr":"ABC123"}],
+    triggerType: 'lpr',
+    displayData: { lprScore: 0.6981093287467957 },
+    y: 171,
+    // source: '5cf961bfa22ecd316bc66390'} //STRAX Rear Parking Lot - staging 
+    // source: '5d0cd9468ce014002543eb16' } //logan - local videofeed - uav - eric-rtmp
+    // source: '5cc0d1eeec485700294b3b6a' } //logan - local videofeed - ip camera
+    source: '5d2a3b0a2676b75da8ba9906' } //sandbox feed - rear parking lot
+  ]
+
+// after connect
+client.on('connect', () => {
+
+  hit.timeStamp = new Date();
+
+  client.publish(CAPTURE_TOPIC, JSON.stringify(hit))
+  console.log('published')
+
+  setInterval(()=>{
+    client.publish(CAPTURE_TOPIC, JSON.stringify(hit))
+    console.log('published')
+  },6000)
+  
+});
